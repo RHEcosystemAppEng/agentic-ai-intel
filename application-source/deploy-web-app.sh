430f9258bfbe11b06d22d78df23a5b1a09654858
Peter Samouelian <psamouel@redhat.com>
2025-06-08 21:29:50 -0400

    commit out helm install.

--- application-source/deploy-web-app.sh
+++ application-source/deploy-web-app.sh
@@ -5,5 +5,5 @@ podman build --no-cache -t agentic-ai-query .
 podman tag localhost/agentic-ai-query:latest docker.io/psamouelian/agentic-ai-query:latest
 podman push docker.io/psamouelian/agentic-ai-query:latest
 
-helm uninstall agentic-ai-query --ignore-not-found
-helm install agentic-ai-query .
+#helm uninstall agentic-ai-query --ignore-not-found
+#helm install agentic-ai-query .
