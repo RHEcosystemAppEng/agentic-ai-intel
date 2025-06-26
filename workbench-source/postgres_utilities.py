dcec2052ead86abadf1427dffa098f9ebc353ee0
Peter Samouelian <psamouel@redhat.com>
2025-06-26 19:12:51 +0000

    Updates from intel ahead.

--- workbench-source/postgres_utilities.py
+++ workbench-source/postgres_utilities.py
@@ -9,23 +9,10 @@ def get_db_connection_uri():
 
     return uri
 
-def get_db_connection_string():
-
-    postgres_host_port = os.getenv("EDB_AIDB_PORT")
-    postgres_host_port = postgres_host_port.replace("tcp://", "")
-    postgres_host_port = postgres_host_port.replace("http://", "")
-    
-    postgres_user = os.getenv("DATABASE_USER")
-    postgres_pwd = os.getenv("DATABASE_PASSWORD")
-    postgres_db_name = os.getenv("DATABASE_NAME")
-
-    postgres_uri = f"postgresql://{postgres_user}:{postgres_pwd}@{postgres_host_port}/{postgres_db_name}"
-
-    return postgres_uri
 
 def create_db_connection():
 
-    postgres_uri = get_db_connection_string()
+    postgres_uri = get_db_connection_uri()
     
     conn = psycopg2.connect(postgres_uri)
 
